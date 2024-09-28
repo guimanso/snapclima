63b80d0c5c6c1a5b287181b8d6b12c05161004bb
guimanso <gui.2015.mv@gmail.com>
2024-09-27 21:22:18 -0300

    JAVASCRIPT FINALIZADO - 27/09/2024

--- snapclima/assets/script/snap.js
+++ snapclima/assets/script/snap.js
@@ -18,35 +18,80 @@ const api_key = "bb232fa4e7dbca936eea6aeb21ee9765";
 
 //`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`
 
-citySearchButton.addEventListener('click', (e) => {
+citySearchButton.addEventListener('click', () => {
     let cityName = citySearchBar.value;
     getCityWeather(cityName);
 });
 
+navigator.geolocation.getCurrentPosition(
+    (position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        getCurrentLocationWeather(lat, lon);
+    },
+    (err) => {
+        if(err.code === 1) {
+            alert('Localização negada pelo usuário. Busque manualmente pela barra de pesquisa.');
+        } else{
+            console.log(err);
+        }
+    }
+);
+
+function getCurrentLocationWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${api_key}`)
+    .then((response) => response.json()) //then = então converte a api em json
+    .then((data) => displayWeather(data))
+}
+
 function getCityWeather(cityName) {
+    weatherIcon.src = `../imgs/loading-icon.svg`
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`)
-        .then((response) => response.json()) //then = então converte a api em json
+        .then((response) => response.json()) 
         .then((data) => displayWeather(data))
 }
+
+
 
 function displayWeather(data) {
     let {
         dt,
         name,
-        weather: [{ incon, description }],
+        weather: [{ icon, description }],
         main: { temp, feels_like, humidity },
         wind: { speed },
         sys: { sunrise, sunset },
     } = data
 
-    currentDate.textContent = dt; // textContent != "innerHTML"
+    console.log(speed)
+
+    currentDate.textContent = formatDate(dt); // textContent != "innerHTML"
     cityName.textContent = name;
 
+    weatherIcon.src = `../imgs/${icon}.svg`
     weatherDescription.textContent = description;
-    currentTemperature.textContent = temp;
-    windSpeed.textContent = speed;
-    feelsLikeTemperature.textContent = feels_like;
-    currentHumidity.textContent = humidity;
-    sunriseTime.textContent = sunrise;
-    sunsetTime.textContent = sunset;
+    currentTemperature.textContent = `${Math.round(temp)}°C`;
+    windSpeed.textContent = `${Math.round(speed * 3.6)} km`;
+    feelsLikeTemperature.textContent = `${Math.round(feels_like)}°C`;
+    currentHumidity.textContent = `${humidity}%`;
+    sunriseTime.textContent = formaTime(sunrise);
+    sunsetTime.textContent = formaTime(sunset);
+}
+
+function zeroEsquerda(num){
+    return num >= 10 ? num : `0${num}` ;
+}
+
+function formatDate(epochTime) {
+    let date = new Date(epochTime * 1000);
+    let formattedDate = date.toLocaleDateString('pt-br', { month: 'long', day: 'numeric'})
+    return `Hoje, ${formattedDate}`; 
+}
+
+function formaTime(epochTime) {
+    let date = new Date(epochTime * 1000);
+    let hours = zeroEsquerda(date.getHours());
+    let minutes = zeroEsquerda(date.getMinutes());
+    return `${hours}:${minutes}`
 }
